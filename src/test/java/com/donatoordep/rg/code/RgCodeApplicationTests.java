21076cbbcf583ceae067e327a4dff55fa211b8e7
zilohnoji <donatoordep@gmail.com>
2024-07-08 18:58:37 -0300

    first commit, project base created

---
+++ src/test/java/com/donatoordep/rg/code/RgCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donatoordep.rg.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RgCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
