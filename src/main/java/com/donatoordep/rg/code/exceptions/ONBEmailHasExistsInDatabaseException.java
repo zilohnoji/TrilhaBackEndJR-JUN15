20ddc7e073289bf779430dcd23dfa0ce7df11df6
zilohnoji <donatoordep@gmail.com>
2024-07-12 23:30:19 -0300

    new exception ONBEmailHasExistsException

---
+++ src/main/java/com/donatoordep/rg/code/exceptions/ONBEmailHasExistsInDatabaseException.java
@@ -0,0 +1,25 @@
+package com.donatoordep.rg.code.exceptions;
+
+import com.donatoordep.rg.code.exceptions.base.ONBExceptionSpecification;
+
+public class ONBEmailHasExistsInDatabaseException extends RuntimeException implements ONBExceptionSpecification {
+
+    private static final String ERROR = "email has exists in database";
+    private static final Integer STATUS = 400;
+
+    public ONBEmailHasExistsInDatabaseException() {
+        super(ERROR);
+    }
+
+    public ONBEmailHasExistsInDatabaseException(String error) {
+        super(error);
+    }
+
+    public String getError() {
+        return ERROR;
+    }
+
+    public Integer getStatus() {
+        return STATUS;
+    }
+}
